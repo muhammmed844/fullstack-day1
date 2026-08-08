f05d70f6992815ca511d2e6e453eb678ebfef8ea
Muhammed Sahil <muhammedsahil844@gmail.com>
2026-08-08 19:53:02 +0530

    Day 5 Express GET and POST routes

--- server.js
+++ server.js
@@ -3,9 +3,42 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+
+app.use(express.json());
+
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to my Blog Backend!");
 });
+
+
+app.get("/blogs", (req, res) => {
+    res.json([
+        {
+            title: "My First Blog",
+            author: "Sahil",
+            content: "This is my first blog post."
+        },
+        {
+            title: "Learning Express.js",
+            author: "Sahil",
+            content: "Today I learned about GET and POST routes."
+        }
+    ]);
+});
+
+
+app.post("/blogs", (req, res) => {
+    const blog = req.body;
+
+    console.log("New blog received:", blog);
+
+    res.status(201).json({
+        message: "Blog added successfully!",
+        blog: blog
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
